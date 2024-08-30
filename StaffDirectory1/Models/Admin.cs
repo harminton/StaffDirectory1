c83f61d682022f3e4a73eccba1e723697b21b9b2
Harminton Villarreal Vasquez <ac150559@avcol.school.nz>
2024-08-30 14:14:36 +1200

    adding admin

---
+++ StaffDirectory1/Models/Admin.cs
@@ -0,0 +1,9 @@
+﻿namespace StaffDirectory1.Models
+{
+    public class Admin
+    {
+        public int AdminID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
