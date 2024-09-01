6baef97c924b72f5e7b0785163b829de8262f74e
Harminton Villarreal Vasquez <ac150559@avcol.school.nz>
2024-09-02 11:59:50 +1200

    added authentication

--- StaffDirectory1/Models/Admin.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StaffDirectory1.Models
-{
-    public class Admin
-    {
-        public int AdminID { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-    }
-}
---
+++ StaffDirectory1/Models/Authentication/Authentication.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace StaffDirectory1.Models.Authentication
+{
+    public class Authentication
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly IConfiguration _configuration;
+
+    }
+}
