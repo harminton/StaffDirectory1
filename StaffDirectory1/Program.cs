a9cdee41643a853859a2faf1effa7b9b8c3cbdaa
Harminton Villarreal Vasquez <ac150559@avcol.school.nz>
2024-09-04 10:11:00 +1200

    Adding Roles

--- StaffDirectory1/Program.cs
+++ StaffDirectory1/Program.cs
@@ -1,20 +1,37 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
 using StaffDirectory1.Areas.Identity.Data;
 using StaffDirectory1.Models;
+using static System.Formats.Asn1.AsnWriter;
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("StaffContextConnection") ?? throw new InvalidOperationException("Connection string 'StaffContextConnection' not found.");
 
 builder.Services.AddDbContext<StaffContext>(options =>
     options.UseSqlServer(connectionString));
 
+//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+
+
 builder.Services.AddDefaultIdentity<StaffUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<StaffContext>();
-    
+    .AddEntityFrameworkStores<StaffContext>()
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<StaffContext>().AddDefaultTokenProviders();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews();
+builder.Services.AddHealthChecks();
+builder.Services.AddRazorPages();
+builder.Services.ConfigureApplicationCookie(options =>
+{
 
+    options.LoginPath = "/Identity/Account/Login";
+    options.AccessDeniedPath = "/Identity/Account/AccesDenied";
+
+
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -39,4 +56,12 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
+
+using (var scope = app.Services.CreateScope)())
+        {
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var roles = new[] { "Admin", "Employee", "Customer" };
+}
+
+
 app.Run();
